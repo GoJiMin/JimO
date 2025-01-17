a58603e96599555d9cd0025489484f1b5c735c37
GoJiMin <gojimin3095@gmail.com>
2025-01-17 17:41:30 +0900

    feat: drag 이벤트 구독
    - Page Item Component를 draggable하게 설정.
    - 각 요소는 드래그가 시작되고, 끝나고, 해당 요소를 벗어나거나 들어오는 이벤트를 구독함.
    
    - Page Component의 경우 아이템이 드래그 중일 때와, 드래그한 요소를 놓았을 때의 이벤트를 구독.
    - notifyDragObservers에서 사용할 dragStateListener를 전달.
    - 타겟의 드래그 상태를 출력하도록 콜백 전달.

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,4 +1,4 @@
-import { qs } from "../../utils/helper.js";
+import { on, qs } from "../../utils/helper.js";
 import { BaseComponent, Component } from "../component.js";
 
 export interface Composable {
@@ -6,9 +6,15 @@ export interface Composable {
 }
 
 type OnCloseListener = () => void;
+type DragState = "start" | "end" | "enter" | "leave";
+type OnDragStateListener<T extends Component> = (
+  target: T,
+  state: DragState
+) => void;
 
 interface SectionContainer extends Component, Composable {
   setOnCloseListener(listener: OnCloseListener): void;
+  setOnDragStateListener(listener: OnDragStateListener<SectionContainer>): void;
 }
 
 type SectionContainerConstructor = {
@@ -19,12 +25,14 @@ export class PageItemComponent
   extends BaseComponent<HTMLElement>
   implements SectionContainer
 {
+  private closeButton: HTMLButtonElement;
   private onCloseListener?: OnCloseListener;
+  private dragStateListener?: OnDragStateListener<PageItemComponent>;
 
   constructor() {
     super(
       `
-      <li class="page-item">
+      <li class="page-item" draggable="true" >
         <section class="page-item__body"></section>
         <div class="page-item__controls">
           <button class="item__close">&times;</button>
@@ -33,11 +41,30 @@ export class PageItemComponent
     `
     );
 
-    const closeButton = qs<HTMLButtonElement>(".item__close", this.element);
+    this.closeButton = qs<HTMLButtonElement>(".item__close", this.element);
 
-    closeButton.onclick = () => {
+    this.bindEvents();
+  }
+
+  private bindEvents() {
+    on<HTMLButtonElement, MouseEvent>(this.closeButton, "click", () => {
       this.onCloseListener && this.onCloseListener();
-    };
+    });
+    on<HTMLElement, DragEvent>(this.element, "dragstart", (event) => {
+      this.onDragStart(event);
+    });
+
+    on<HTMLElement, DragEvent>(this.element, "dragend", (event) => {
+      this.onDragEnd(event);
+    });
+
+    on<HTMLElement, DragEvent>(this.element, "dragenter", (event) => {
+      this.onDragEnter(event);
+    });
+
+    on<HTMLElement, DragEvent>(this.element, "dragleave", (event) => {
+      this.onDragLeave(event);
+    });
   }
 
   addChild(child: Component): void {
@@ -48,6 +75,30 @@ export class PageItemComponent
   setOnCloseListener(listener: OnCloseListener) {
     this.onCloseListener = listener;
   }
+
+  setOnDragStateListener(listener: OnDragStateListener<PageItemComponent>) {
+    this.dragStateListener = listener;
+  }
+
+  private onDragStart(_: DragEvent) {
+    this.notifyDragObservers("start");
+  }
+
+  private onDragEnd(_: DragEvent) {
+    this.notifyDragObservers("end");
+  }
+
+  private onDragEnter(_: DragEvent) {
+    this.notifyDragObservers("enter");
+  }
+
+  private onDragLeave(_: DragEvent) {
+    this.notifyDragObservers("leave");
+  }
+
+  private notifyDragObservers(state: DragState) {
+    this.dragStateListener && this.dragStateListener(this, state);
+  }
 }
 
 export class PageComponent
@@ -56,6 +107,18 @@ export class PageComponent
 {
   constructor(private pageItemConstructor: SectionContainerConstructor) {
     super(`<ul class="page"></ul>`);
+
+    this.bindEvents();
+  }
+
+  private bindEvents() {
+    on<HTMLUListElement, DragEvent>(this.element, "dragover", (event) => {
+      this.onDragOver(event);
+    });
+
+    on<HTMLUListElement, DragEvent>(this.element, "drop", (event) => {
+      this.onDrop(event);
+    });
   }
 
   addChild(child: Component): void {
@@ -66,5 +129,22 @@ export class PageComponent
     item.setOnCloseListener(() => {
       item.removeFrom(this.element);
     });
+    item.setOnDragStateListener(
+      (target: SectionContainer, state: DragState) => {
+        console.log(target, state);
+      }
+    );
+  }
+
+  private onDragOver(event: DragEvent) {
+    event.preventDefault();
+
+    console.log("Drag Over");
+  }
+
+  private onDrop(event: DragEvent) {
+    event.preventDefault();
+
+    console.log("Drop");
   }
 }
